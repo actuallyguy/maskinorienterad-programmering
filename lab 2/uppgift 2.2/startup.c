0bf15ed218ab99709084ab9307d62d61c424fc84
actuallyguy <82503899+actuallyguy@users.noreply.github.com>
2023-01-23 15:27:07 +0100

    Update startup.c

--- lab 2/uppgift 2.2/startup.c
+++ lab 2/uppgift 2.2/startup.c
@@ -1,11 +1,10 @@
-__attribute__((naked))
-__attribute__((section(".start_section")))
-void startup(void)
+__attribute__((naked)) __attribute__((section (".start_section")) )
+void startup ( void )
 {
-    __asm__ volatile(" LDR R0,=0x2001C000\n");
-    __asm__ volatile(" MOV SP,R0\n");
-    __asm__ volatile(" BL main\n");
-    __asm__ volatile(" B .\n");
+__asm__ volatile(" LDR R0,=0x2001C000\n");		/* set stack */
+__asm__ volatile(" MOV SP,R0\n");
+__asm__ volatile(" BL main\n");					/* call main */
+__asm__ volatile(".L1: B .L1\n");				/* never return */
 }
 
 #define SIMULATOR
