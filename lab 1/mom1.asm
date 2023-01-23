38ac6f86c70c5fead1d89e1064dd10b812091067
actuallyguy <82503899+actuallyguy@users.noreply.github.com>
2023-01-23 13:27:16 +0100

    screwed up tabbing

--- lab 1/mom1.asm
+++ lab 1/mom1.asm
@@ -1,9 +1,9 @@
-start:	LDR	R0,=0x55555555
-		LDR	R1,=0x40020C00	@ config port D
-		STR R0,[R1]
-		LDR R1,=0x40020C14  @ outport port D
-		LDR R2,=0x40021010  @ inport port E
+start:	LDR R0,=0x55555555
+	LDR R1,=0x40020C00	@ config port D
+	STR R0,[R1]
+	LDR R1,=0x40020C14  @ outport port D
+	LDR R2,=0x40021010  @ inport port E
 
-main: 	LDR R0,[R2]
-		STR R0,[R1]
-		B main
+main:	LDR R0,[R2]
+	STR R0,[R1]
+	B main
