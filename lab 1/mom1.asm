a3a11cfab242cad93d63962ee51e4481719e38c4
actuallyguy <82503899+actuallyguy@users.noreply.github.com>
2023-03-21 16:24:24 +0100

    wrong file, reupload for completeness

--- lab 1/mom1.asm
+++ lab 1/mom1.asm
@@ -1,9 +1,15 @@
-start:	LDR R0,=0x55555555
-	LDR R1,=0x40020C00	@ config port D
-	STR R0,[R1]
-	LDR R1,=0x40020C14  @ outport port D
-	LDR R2,=0x40021010  @ inport port E
+start:  
+    LDR R0,=0x18        @lägg till din kod här
+    LDR R1,=0x40023830
+    STR R0,[R1]         @ aktivera portar D och E
+    
+    LDR R0,=0x55555555
+    LDR R1,=0x40020C00  @ config port D
+    STR R0,[R1]
+    LDR R1,=0x40020C14  @ outport port D
+    LDR R2,=0x40021010  @ inport port E
 
-main:	LDR R0,[R2]
-	STR R0,[R1]
-	B main
+main:   
+    LDR R0,[R2]
+    STR R0,[R1]
+    B main
